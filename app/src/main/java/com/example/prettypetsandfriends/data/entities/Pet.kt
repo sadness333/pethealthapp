643b213dacf813610e64ef685c4668f7e924bcd7
sadness <dobrokn@gmail.com>
2025-04-01 19:47:57 +0300

    1.002 v || исправления с изменением бд

--- app/src/main/java/com/example/prettypetsandfriends/data/entities/Pet.kt
+++ app/src/main/java/com/example/prettypetsandfriends/data/entities/Pet.kt
@@ -1,6 +1,5 @@
 package com.example.prettypetsandfriends.data.entities
 
-import java.util.UUID
 
 data class Pet(
     val id: String = "",
@@ -8,31 +7,12 @@ data class Pet(
     val type: String = "",
     val breed: String = "",
     val age: Int = 0,
+    val weight: Double = 0.0,
     val ownerId: String = "",
     val photoUrl: String? = "",
-    val statistics: PetStatistics = PetStatistics(),
     val nutrition: PetNutrition = PetNutrition(),
-    val vaccinations: List<Vaccination> = emptyList()
+    val weightHistory: Map<String, WeightHistory> = emptyMap()
 ) {
-    data class Vaccination(
-        val id: String = "",
-        val petId: String = "",
-        val name: String = "",
-        val date: Long = 0L,
-        val nextDate: Long = 0L,
-        val vetPerson: String = ""
-    )
-
-    data class PetStatistics(
-        val vetVisitsCount: Int = 0,
-        val avgHealthScore: Double = 0.0,
-        val lastHealthScore: Int = 0,
-        val lastWeight: Double = 0.0,
-        val avgWeight: Double = 0.0,
-        val lastVetVisit: Long = 0,
-        val vaccinationsCount: Int = 0,
-        val feedingConsistency: Int = 0
-    )
 
     data class PetNutrition(
         val dailyCalories: Int = 0,
